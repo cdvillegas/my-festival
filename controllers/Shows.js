87085eb8df12a99964e1ee3f9a03bd10e20134dd
cdvillegas <cdvillegas11@gmail.com>
2024-02-14 00:03:49 -0800

    Removed unecessary comment

--- controllers/Shows.js
+++ controllers/Shows.js
@@ -24,10 +24,8 @@ module.exports.delete_show = function delete_show (req, res, next, id) {
 };
 
 module.exports.list_shows = function list_shows(req, res, next) {
-  // Extract 'festival_id' from the query parameters
   const festival_id = req.query.festival_id;
 
-  // Now pass this 'festival_id' to your ShowsService, ensuring it's designed to accept this parameter
   Shows.list_shows(festival_id)
     .then(function(response) {
       utils.writeJson(res, response);
